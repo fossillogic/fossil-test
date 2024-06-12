748fcff85241ffd8816c10f6d027df0d487fc9a3
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-06-12 07:44:48 -0500

    Update unittest.h

--- code/include/fossil/unittest.h
+++ code/include/fossil/unittest.h
@@ -332,7 +332,7 @@ extern "C"
  * 
  * @param description The description of the GIVEN condition.
  */
-#define GIVEN(description) fossil_test_cout("magenta", "%s%s\n", "          : GIVEN ", description); if (true)
+#define GIVEN(description) _GIVEN(description)
 
 /**
  * @brief Define macro for specifying a WHEN condition in a test scenario.
@@ -345,7 +345,7 @@ extern "C"
  * 
  * @param description The description of the WHEN condition.
  */
-#define WHEN(description) fossil_test_cout("magenta", "%s%s\n", "          : \tWHEN ", description); if (true)
+#define WHEN(description) _WHEN(description)
 
 /**
  * @brief Define macro for specifying a THEN condition in a test scenario.
@@ -357,7 +357,7 @@ extern "C"
  * 
  * @param description The description of the THEN condition.
  */
-#define THEN(description) fossil_test_cout("magenta", "%s%s\n", "          : \t\tTHEN ", description); if (true)
+#define THEN(description) _THEN(description)
 
 // =================================================================
 // TDD specific commands
