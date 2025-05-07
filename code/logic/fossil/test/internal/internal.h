84ab977faff515236eafb3f89fda92d280fe6665
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-05-07 11:59:29 -0500

    wire the internal source

--- code/logic/fossil/test/internal/internal.h
+++ code/logic/fossil/test/internal/internal.h
@@ -28,24 +28,6 @@
 #include <time.h>
 #include <float.h>
 
-// Define color codes for output
-#define FOSSIL_TEST_COLOR_RESET       "\033[0m"
-#define FOSSIL_TEST_COLOR_RED         "\033[31m"
-#define FOSSIL_TEST_COLOR_GREEN       "\033[32m"
-#define FOSSIL_TEST_COLOR_YELLOW      "\033[33m"
-#define FOSSIL_TEST_COLOR_BLUE        "\033[34m"
-#define FOSSIL_TEST_COLOR_MAGENTA     "\033[35m"
-#define FOSSIL_TEST_COLOR_CYAN        "\033[36m"
-#define FOSSIL_TEST_COLOR_WHITE       "\033[37m"
-
-// Define text attributes
-#define FOSSIL_TEST_ATTR_BOLD         "\033[1m"
-#define FOSSIL_TEST_ATTR_UNDERLINE    "\033[4m"
-#define FOSSIL_TEST_ATTR_REVERSED     "\033[7m"
-#define FOSSIL_TEST_ATTR_BLINK        "\033[5m"
-#define FOSSIL_TEST_ATTR_HIDDEN       "\033[8m"
-#define FOSSIL_TEST_ATTR_NORMAL       "\033[22m"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -98,8 +80,6 @@ typedef enum {
  * @var fossil_test_options_t::quiet
  * Flag to suppress most non-essential output for minimal console logging.
  * 
- * @var fossil_test_options_t::color_output
- * Flag to enable or disable colorized output in the console.
  */
 typedef struct {
     bool show_version;
@@ -110,52 +90,10 @@ typedef struct {
     int32_t repeat_count;
     bool shuffle_enabled;
     bool dry_run;
-    bool color_output;
     fossil_test_summary_t summary;     // Replaces 'quiet'
     fossil_test_format_t format;       // Store the format type
 } fossil_test_options_t;
 
-/**
- * Prints a string to the output.
- *
- * @param str The string to be printed.
- */
-void internal_test_puts(const char *str);
-
-/**
- * Prints a formatted string to the output.
- *
- * @param format The format string.
- * @param ... The additional arguments to be formatted.
- */
-void internal_test_printf(const char *format, ...);
-
-/**
- * Prints a string to the output with a specified color.
- *
- * @param color The color code to be applied.
- * @param format The format string.
- * @param ... The additional arguments to be formatted.
- */
-void internal_test_print_color(const char *color, const char *format, ...);
-
-/**
- * Prints a character to the output.
- *
- * @param c The character to be printed.
- */
-void internal_test_putchar(char c);
-
-/**
- * Prints a character to the output with a specified color.
- *
- * @param c The character to be printed.
- * @param color The color code to be applied.
- */
-void internal_test_putchar_color(char c, const char *color);
-
-// Set global color output flag
-void internal_test_set_color_output(bool enabled);
 
 #ifdef __cplusplus
 }
