dc919fa14ef8309131e487f479be19b1dd30afa2
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-05-08 19:02:20 -0500

    connect sanity empty shell

---
+++ code/logic/fossil/pizza/sanity.h
@@ -0,0 +1,32 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Project: Fossil Logic
+ *
+ * This file is part of the Fossil Logic project, which aims to develop high-
+ * performance, cross-platform applications and libraries. The code contained
+ * herein is subject to the terms and conditions defined in the project license.
+ *
+ * Author: Michael Gene Brockus (Dreamer)
+ * Date: 04/05/2014
+ *
+ * Copyright (C) 2014-2025 Fossil Logic. All rights reserved.
+ * -----------------------------------------------------------------------------
+ */
+#ifndef FOSSIL_SANITY_H
+#define FOSSIL_SANITY_H
+
+#include "common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// interface
+
+#ifdef __cplusplus
+}
+#endif
+
+// macro interface
+
+#endif // FOSSIL_MOCK_FRAMEWORK_H
---
+++ code/logic/sanity.c
@@ -0,0 +1,17 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Project: Fossil Logic
+ *
+ * This file is part of the Fossil Logic project, which aims to develop high-
+ * performance, cross-platform applications and libraries. The code contained
+ * herein is subject to the terms and conditions defined in the project license.
+ *
+ * Author: Michael Gene Brockus (Dreamer)
+ * Date: 04/05/2014
+ *
+ * Copyright (C) 2014-2025 Fossil Logic. All rights reserved.
+ * -----------------------------------------------------------------------------
+ */
+#include "fossil/pizza/sanity.h"
+
+// TODO define the meaning of sanity as a kit for command-line argument testing
