18df056539e3c8f9d044dadde6153d3d18c5c13b
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-05-14 13:49:18 -0500

    add test cases to mark

--- code/tests/cases/test_mark.c
+++ code/tests/cases/test_mark.c
@@ -51,11 +51,93 @@ FOSSIL_TEST(c_mark_start_and_stop) {
     ASSUME_ITS_EQUAL_CSTR(benchmark_stop_test.name, "stop_test");
 }
 
+// Test case for MARK_BENCHMARK
+FOSSIL_TEST(c_mark_benchmark) {
+    MARK_BENCHMARK(test_benchmark);
+    ASSUME_ITS_EQUAL_CSTR(benchmark_test_benchmark.name, "test_benchmark");
+}
+
+// Test case for MARK_START
+FOSSIL_TEST(c_mark_start) {
+    MARK_BENCHMARK(start_test);
+    MARK_START(start_test);
+    ASSUME_ITS_TRUE(benchmark_start_test.running);
+}
+
+// Test case for MARK_STOP
+FOSSIL_TEST(c_mark_stop) {
+    MARK_BENCHMARK(stop_test);
+    MARK_START(stop_test);
+    MARK_STOP(stop_test);
+    ASSUME_ITS_FALSE(benchmark_stop_test.running);
+}
+
+// Test case for MARK_BENCHMARK with multiple benchmarks
+FOSSIL_TEST(c_mark_multiple_benchmarks) {
+    MARK_BENCHMARK(benchmark1);
+    MARK_BENCHMARK(benchmark2);
+    ASSUME_ITS_EQUAL_CSTR(benchmark_benchmark1.name, "benchmark1");
+    ASSUME_ITS_EQUAL_CSTR(benchmark_benchmark2.name, "benchmark2");
+}
+
+// Test case for MARK_START and MARK_STOP with elapsed time
+FOSSIL_TEST(c_mark_elapsed_time) {
+    MARK_BENCHMARK(elapsed_test);
+    MARK_START(elapsed_test);
+    // Simulate some work
+    for (volatile int i = 0; i < 1000000; ++i);
+    MARK_STOP(elapsed_test);
+    ASSUME_ITS_TRUE(benchmark_elapsed_test.total_duration > 0.0);
+}
+
+// Test case for MARK_BENCHMARK with invalid name
+FOSSIL_TEST(c_mark_invalid_benchmark_name) {
+    MARK_BENCHMARK(null_benchmark);
+    ASSUME_NOT_CNULL(benchmark_null_benchmark.name);
+}
+
+// Test case for MARK_STOP without MARK_START
+FOSSIL_TEST(c_mark_stop_without_start) {
+    MARK_BENCHMARK(stop_without_start);
+    MARK_STOP(stop_without_start);
+    ASSUME_ITS_FALSE(benchmark_stop_without_start.running);
+}
+
+// Test case for MARK_BENCHMARK with nested benchmarks
+FOSSIL_TEST(c_mark_nested_benchmarks) {
+    MARK_BENCHMARK(outer_benchmark);
+    MARK_START(outer_benchmark);
+    MARK_BENCHMARK(inner_benchmark);
+    MARK_START(inner_benchmark);
+    MARK_STOP(inner_benchmark);
+    MARK_STOP(outer_benchmark);
+    ASSUME_ITS_TRUE(benchmark_outer_benchmark.total_duration >= benchmark_inner_benchmark.total_duration);
+}
+
+// Test case for resetting a benchmark
+FOSSIL_TEST(c_mark_reset_benchmark) {
+    MARK_BENCHMARK(reset_test);
+    MARK_START(reset_test);
+    MARK_STOP(reset_test);
+    fossil_benchmark_reset(&benchmark_reset_test);
+    ASSUME_ITS_EQUAL_I32(benchmark_reset_test.total_duration, 0.0);
+    ASSUME_ITS_EQUAL_I32(benchmark_reset_test.num_samples, 0);
+}
+
 // * * * * * * * * * * * * * * * * * * * * * * * *
 // * Fossil Logic Test Pool
 // * * * * * * * * * * * * * * * * * * * * * * * *
 FOSSIL_TEST_GROUP(c_mark_test_cases) {
     FOSSIL_TEST_ADD(c_mark_suite, c_mark_start_and_stop);
+    FOSSIL_TEST_ADD(c_mark_suite, c_mark_benchmark);
+    FOSSIL_TEST_ADD(c_mark_suite, c_mark_start);
+    FOSSIL_TEST_ADD(c_mark_suite, c_mark_stop);
+    FOSSIL_TEST_ADD(c_mark_suite, c_mark_multiple_benchmarks);
+    FOSSIL_TEST_ADD(c_mark_suite, c_mark_elapsed_time);
+    FOSSIL_TEST_ADD(c_mark_suite, c_mark_invalid_benchmark_name);
+    FOSSIL_TEST_ADD(c_mark_suite, c_mark_stop_without_start);
+    FOSSIL_TEST_ADD(c_mark_suite, c_mark_nested_benchmarks);
+    FOSSIL_TEST_ADD(c_mark_suite, c_mark_reset_benchmark);
 
     FOSSIL_TEST_REGISTER(c_mark_suite);
 }
