ae77d2adfbf12f49dac0f86f039b8dd13d3ca300
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-05-14 15:36:12 -0500

    apply logic to handle windows and linux systems

--- code/logic/fossil/pizza/test.h
+++ code/logic/fossil/pizza/test.h
@@ -545,8 +545,13 @@ void _on_skip(const char *description);
     * runner. If the condition is false, the test runner will output the specified
     * message and may abort the execution of the test case or test suite.
     */ 
+#ifdef _WIN32
+#define _FOSSIL_TEST_ASSUME_MESSAGE(message, ...) \
+    pizza_test_assert_messagef((message), __VA_ARGS__)
+#else
 #define _FOSSIL_TEST_ASSUME_MESSAGE(message, ...) \
     pizza_test_assert_messagef((message) __VA_OPT__(, __VA_ARGS__))
+#endif
 
 /**
  * @brief Macro for defining a Given step in a behavior-driven development test.
