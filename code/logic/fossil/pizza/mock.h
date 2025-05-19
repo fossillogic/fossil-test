cd89bca1123fc0371d631789e93eeee87e3020ec
Michael Gene Brockus (Dreamer) <michaelbrockus@gmail.com>
2025-05-19 18:37:44 -0500

    update macro logic

--- code/logic/fossil/pizza/mock.h
+++ code/logic/fossil/pizza/mock.h
@@ -245,15 +245,10 @@ bool fossil_mock_compare_output(const char *captured, const char *expected);
     fossil_mock_capture_output(buffer, size, function, __VA_ARGS__)
 #elif defined(__APPLE__)
 #define _FOSSIL_MOCK_CAPTURE_OUTPUT(buffer, size, function, ...) \
-    fossil_mock_capture_output(buffer, size, function, __VA_OPT(, __VA_ARGS__))
-#else
-#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201710L
-#define _FOSSIL_MOCK_CAPTURE_OUTPUT(buffer, size, function, ...) \
-    fossil_mock_capture_output(buffer, size, function, __VA_OPT(, __VA_ARGS__))
+    fossil_mock_capture_output(buffer, size, function, ##__VA_ARGS__)
 #else
 #define _FOSSIL_MOCK_CAPTURE_OUTPUT(buffer, size, function, ...) \
-    fossil_mock_capture_output(buffer, size, function, __VA_ARGS__)
-#endif
+    fossil_mock_capture_output(buffer, size, function, ##__VA_ARGS__)
 #endif
 
 /**
@@ -372,13 +367,8 @@ bool fossil_mock_compare_output(const char *captured, const char *expected);
 #define FOSSIL_MOCK_REDIRECT_STDOUT(buffer, size, function, ...) \
     _FOSSIL_MOCK_CAPTURE_OUTPUT(buffer, size, function, __VA_OPT(, __VA_ARGS__))
 #else
-#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 201710L
 #define FOSSIL_MOCK_REDIRECT_STDOUT(buffer, size, function, ...) \
-    _FOSSIL_MOCK_CAPTURE_OUTPUT(buffer, size, function, __VA_OPT(, __VA_ARGS__))
-#else
-#define FOSSIL_MOCK_REDIRECT_STDOUT(buffer, size, function, ...) \
-    _FOSSIL_MOCK_CAPTURE_OUTPUT(buffer, size, function, __VA_ARGS__)
-#endif
+    _FOSSIL_MOCK_CAPTURE_OUTPUT(buffer, size, function, ##__VA_ARGS__)
 #endif
 
 /**
